e3c00105f692f0badaa089453d953f63710d0734
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-04-15 20:47:49 +0100

    removed hard coded buttons from html and added buttons dynamically with in app.js

--- app.js
+++ app.js
@@ -72,37 +72,25 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const selectionCenter = document.querySelector(".section-center");
 
-const filterBtns = document.querySelectorAll(".filter-btn");
+const btnsContainer = document.querySelector(".btn-container");
 
 // listen for DOM content to be loaded
 //load items
 window.addEventListener("DOMContentLoaded", function () {
   displayMenuItems(menu);
-});
-
-// filter items
-filterBtns.forEach(function (btn) {
-  btn.addEventListener("click", function (event) {
-    console.log(event.currentTarget.dataset.id);
-    const category = event.currentTarget.dataset.id;
-    const menuCategory = menu.filter(function (menuItem) {
-      console.log(menuItem.category);
-      if (menuItem.category === category) {
-        return menuItem;
-      }
-    });
-    console.log(menuCategory);
-    if (category === "all") {
-      console.log("All!!!!!");
-      displayMenuItems(menu);
-    } else {
-      displayMenuItems(menuCategory);
-    }
-  });
+  displayMenuBtns();
 });
 
 function displayMenuItems(menuItems) {
@@ -124,4 +112,48 @@ function displayMenuItems(menuItems) {
 
   displayMenu = displayMenu.join(""); // make string from array
   selectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuBtns() {
+  // iterate over the menu array & return only unique categories
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  console.log(categories);
+  // create buttons for each category
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button class="filter-btn" type="button" data-id=${category}>
+  ${category}
+</button>`;
+    })
+    .join("");
+  console.log(categoryBtns);
+  btnsContainer.innerHTML = categoryBtns;
+  const filterBtns = btnsContainer.querySelectorAll(".filter-btn");
+  // filter items
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (event) {
+      //console.log(event.currentTarget.dataset.id);
+      const category = event.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        //console.log(menuItem.category);
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      //console.log(menuCategory);
+      if (category === "all") {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
 }
